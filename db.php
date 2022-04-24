f9ccef15b1f9c0ebac07ec9e8d62725cb2698196
Antonio Reda <70113532+AntoReda@users.noreply.github.com>
2022-04-24 17:24:41 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php 
 
 /** make the data secure  */
-
-$db['db_host'] = "localhost"; 
+//The host and password needs to be altered if you have a password for phpmyadmin set and a different port number for your server.
+$db['db_host'] = "localhost"; //if your server port number is for example 3308 you should write between the "" localhost:3308.
 $db['db_user'] = "root";
-$db['db_pass'] = "";
+$db['db_pass'] = "";//If your phpmyadmin has a password add it between the "".
 $db['db_name'] = "verdi";
 
 foreach($db as $key => $value){
